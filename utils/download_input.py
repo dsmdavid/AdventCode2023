973ef2deaad019411fc5e2e72f82754630598b7d
dsmdavidIL <61737063+dsmdavidIL@users.noreply.github.com>
2023-12-04 13:50:52 +0000

    fix: update folder to download inputs

--- utils/download_input.py
+++ utils/download_input.py
@@ -102,7 +102,7 @@ def download_input(filename=None, session=None):
 URL = "https://adventofcode.com/{:d}/day/{:d}/{:s}"
 YEAR = -1
 DAY = -1
-DOWNLOAD_FOLDER = "./inputs"
+DOWNLOAD_FOLDER = "../inputs"
 AOC_USER = os.getenv("AOC_USER")
 AOC_EMAIL = os.getenv("AOC_EMAIL")
 AOC_USER_AGENT = os.getenv("AOC_USER_AGENT")
